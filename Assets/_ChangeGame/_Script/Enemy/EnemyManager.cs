2eeb033f0ba6ba97590d3f0f5f442f17f05c18d7
Pureria <117608289+Pureria@users.noreply.github.com>
2024-03-24 07:04:32 +0900

    EnemyManager作成

--- Assets/_ChangeGame/_Script/Enemy/EnemyManager.cs
+++ Assets/_ChangeGame/_Script/Enemy/EnemyManager.cs
@@ -9,12 +9,17 @@ namespace ChangeGame.Enemy
     {
         //プレハブを格納する変数
         [SerializeField] private List<GameObject> _enemyPrefab = new List<GameObject>();    //敵のプレハブリスト
-        [SerializeField] private List<GameObject> _enemySpawnPoint = new List<GameObject>();   //SpawnPointのリスト
+        [SerializeField] private Transform _spawnPointParent;   //敵の生成位置を格納する親オブジェクト
         [SerializeField] private int _maxEnemyCount = 50;   //生成する敵の最大数
         [SerializeField] private float _spawnInterval = 40.0f;
+        [SerializeField] private Transform _enemyParent;
+
+        [Header("Debug")] 
+        [SerializeField] private Transform _goal;
 
         private float _nextSpawnTime;
-        private List<Transform> _EnemyInstaces = new List<Transform>(); //生成された敵のTransformを格納するリスト
+        private List<EnemyController> _EnemyInstaces = new List<EnemyController>(); //生成された敵のTransformを格納するリスト
+        private List<Transform> _spawnPoints = new List<Transform>(); //SpawnPointのTransformを格納するリスト
 
         private void Start()
         {
@@ -24,18 +29,36 @@ namespace ChangeGame.Enemy
                 for (int j = 0; j < _maxEnemyCount / _enemyPrefab.Count; j++)
                 {
                     //このオブジェクトの子オブジェクトとして敵を生成
-                    GameObject enemy = Instantiate(_enemyPrefab[i], transform);
-                    _EnemyInstaces.Add(enemy.transform);
-                    enemy.SetActive(false);
+                    GameObject enemy = Instantiate(_enemyPrefab[i], _enemyParent.transform);
+                    if (enemy.TryGetComponent<EnemyController>(out EnemyController ec))
+                    {
+                        _EnemyInstaces.Add(ec);
+                    }
+                    else
+                    {
+                        Debug.LogError("EnemyControllerがアタッチされていません");
+                        Destroy(enemy);
+                    }
                 }
             }
 
+            //_spawnPointParentの子オブジェクトを取得して_spawnPointsに格納
+            foreach (Transform spawnPoint in _spawnPointParent)
+            {
+                _spawnPoints.Add(spawnPoint);
+            }
+
+            //TODO::ゲーム開始の時間を入れるように
+            _nextSpawnTime = Time.time + _spawnInterval;
         }
 
         private void Update()
         {
-
-
+            if (_nextSpawnTime <= Time.time)
+            {
+                _nextSpawnTime = Time.time + _spawnInterval;
+                SpawnEnemy();
+            }
         }
 
         private void SpawnEnemy()
@@ -45,9 +68,11 @@ namespace ChangeGame.Enemy
             {
                 if (!_EnemyInstaces[i].gameObject.activeSelf)
                 {
-                    int index = Random.Range(0, _enemySpawnPoint.Count);
-                    _EnemyInstaces[i].position = _enemySpawnPoint[index].transform.position;
+                    int index = Random.Range(0, _spawnPoints.Count);
+                    _EnemyInstaces[i].transform.position = _spawnPoints[index].transform.position;
                     _EnemyInstaces[i].gameObject.SetActive(true);
+                    //TODO::プレイヤーのTransformを渡すように
+                    _EnemyInstaces[i].Initialize(_goal.transform);
                     break;
                 }
             }  
