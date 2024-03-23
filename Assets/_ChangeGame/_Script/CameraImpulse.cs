66ee643423ee3d768fa4a77243efd911d174d0c3
siroemon <k4jgds@gmail.com>
2024-03-24 07:04:06 +0900

    CameraImpulse.csを編集

---
+++ Assets/_ChangeGame/_Script/CameraImpulse.cs
@@ -0,0 +1,40 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraImpulse : MonoBehaviour
+{
+    [SerializeField] private float _smallAmplitude = 1;
+    [SerializeField] private float _middleAmplitude = 2;
+    [SerializeField] private float _largeAmplitude = 3;
+
+    private  CinemachineImpulseSource _source;
+
+    private void Start()
+    {
+        _source = GetComponent<CinemachineImpulseSource>();
+    }
+
+
+    public void SmallImpulse()
+    {
+        _source.m_ImpulseDefinition.m_AmplitudeGain = _smallAmplitude;
+        _source.GenerateImpulse();
+    }
+
+    public void MiddleImpulse()
+    {
+        _source.m_ImpulseDefinition.m_AmplitudeGain = _middleAmplitude;
+        _source.GenerateImpulse();
+    }
+
+    public void LargeImpulse()
+    {
+        _source.m_ImpulseDefinition.m_AmplitudeGain = _largeAmplitude;
+        _source.GenerateImpulse();
+    }
+
+
+}
