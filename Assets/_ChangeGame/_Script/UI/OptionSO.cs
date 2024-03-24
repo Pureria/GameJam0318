eb94d09607b98b4b659c0ab485333b3d068a397a
siroemon <k4jgds@gmail.com>
2024-03-24 13:54:12 +0900

    Optionのスライダー作成

--- Assets/_ChangeGame/_Script/UI/OptionSO.cs
+++ Assets/_ChangeGame/_Script/UI/OptionSO.cs
@@ -8,10 +8,10 @@ namespace ChangeGame.UI
     [CreateAssetMenu(fileName = "OptionSO", menuName = "ChangeGame/UI/OptionSO")]
     public class OptionSO : ScriptableObject
     {
-        public float MouseSensivity;
-        public float MasterVolume;
-        public float BGMVolume;
-        public float SEVolume;
+        public float MouseSensivity = 0f;
+        public float MasterVolume = 0.5f;
+        public float BGMVolume = 0.5f;
+        public float SEVolume = 0.5f;
     }
 
 }
