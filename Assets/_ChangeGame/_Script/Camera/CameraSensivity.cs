e101813f0472dc70e19c03ab9103c85fc7bf1716
Pureria <117608289+Pureria@users.noreply.github.com>
2024-03-24 14:14:25 +0900

    キャラクターのマテリアル編集

---
+++ Assets/_ChangeGame/_Script/Camera/CameraSensivity.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChangeGame.CameraImpulse
+{
+    public class CameraSensivity : MonoBehaviour
+    {
+    }
+}
