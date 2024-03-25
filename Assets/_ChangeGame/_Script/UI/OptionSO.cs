cccc790b8b05f5cf9eb5f37dd61a820ad03f8d0e
siroemon <k4jgds@gmail.com>
2024-03-25 19:17:14 +0900

    初期値の設定ができない不具合を修正

--- Assets/_ChangeGame/_Script/UI/OptionSO.cs
+++ Assets/_ChangeGame/_Script/UI/OptionSO.cs
@@ -9,10 +9,10 @@ namespace ChangeGame.UI
     [CreateAssetMenu(fileName = "OptionSO", menuName = "ChangeGame/UI/OptionSO")]
     public class OptionSO : ScriptableObject
     {
-        public float MouseSensivity = 0f;
-        public float MasterVolume = 0.5f;
-        public float BGMVolume = 0.25f;
-        public float SEVolume = 0.5f;
+        public float MouseSensivity;
+        public float MasterVolume;
+        public float BGMVolume;
+        public float SEVolume;
 
         public Action OnChangeAnyVolumeEvent;
     }
