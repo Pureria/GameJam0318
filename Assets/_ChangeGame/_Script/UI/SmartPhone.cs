cea321b67d679da2f151e36b03cdc6ac5b7ca089
Pureria <117608289+Pureria@users.noreply.github.com>
2024-04-11 00:49:32 +0900

    UI関連

---
+++ Assets/_ChangeGame/_Script/UI/SmartPhone.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine.Serialization;
+
+namespace ChangeGame.UI.SmartPhone
+{
+    public class SmartPhone : MonoBehaviour
+    {
+        [SerializeField] private List<Image> _phoneImages = new List<Image>();
+        [SerializeField] private float _changeTime = 5.0f;
+        [SerializeField] private float _changeInterval = 2.0f;
+        private int _nowImageIndex;
+
+        private void Start()
+        {
+            _nowImageIndex = 0;
+            WaitChangeImage(this.GetCancellationTokenOnDestroy());
+        }
+
+        private async UniTask WaitChangeImage(CancellationToken token)
+        {
+            while (true)
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(_changeInterval), cancellationToken: token);
+                await _phoneImages[_nowImageIndex].DOFade(0, _changeTime);
+                _nowImageIndex++;
+                if (_nowImageIndex >= _phoneImages.Count) _nowImageIndex = 0;
+                await _phoneImages[_nowImageIndex].DOFade(1, _changeTime);
+            }
+        }
+    }
+}
