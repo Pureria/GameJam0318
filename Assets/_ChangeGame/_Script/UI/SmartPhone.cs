6cd96be33e80ffc78374ee0228b390f5fa812f06
Pureria <117608289+Pureria@users.noreply.github.com>
2024-04-12 02:30:39 +0900

    スマホの画面とパーティクル

--- Assets/_ChangeGame/_Script/UI/SmartPhone.cs
+++ Assets/_ChangeGame/_Script/UI/SmartPhone.cs
@@ -20,6 +20,14 @@ namespace ChangeGame.UI.SmartPhone
         private void Start()
         {
             _nowImageIndex = 0;
+            
+            foreach (var image in _phoneImages)
+            {
+                Color color = image.color;
+                color.a = 0;
+                image.color = color;
+            }
+            
             WaitChangeImage(this.GetCancellationTokenOnDestroy()).Forget();
         }
 
@@ -27,11 +35,11 @@ namespace ChangeGame.UI.SmartPhone
         {
             while (true)
             {
+                await _phoneImages[_nowImageIndex].DOFade(1, _changeTime);
                 await UniTask.Delay(TimeSpan.FromSeconds(_changeInterval), cancellationToken: token);
                 await _phoneImages[_nowImageIndex].DOFade(0, _changeTime);
                 _nowImageIndex++;
                 if (_nowImageIndex >= _phoneImages.Count) _nowImageIndex = 0;
-                await _phoneImages[_nowImageIndex].DOFade(1, _changeTime);
             }
         }
     }
